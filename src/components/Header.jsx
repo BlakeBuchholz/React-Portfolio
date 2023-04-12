461a9e87f56f9512451bc4af3d4d3af7eab197ca
BlakeBuchholz <blakebuchholz@gmail.com>
2023-04-12 17:21:21 -0500

    Added Coding Brackets PNG to the Header component and copied the introduction content to it to edit later.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,17 @@ function Header() {
   return (
     <div className='header-Wrapper'>
      <div className='header-Border'></div>
-     <div className='introduction-Section'>
-      <h1 className='introduction-Header'>Hi, I'm Blake</h1>
-      <div className='header-Animation'>
+     <div className='codingBrackets'>
+       <img src='./codingbrackets.png' alt='Coding Brackets' width={600} height={600}></img>
+       <p className='codingBrackets-Content'>I'm an entry-level front-end developer with a passion for creating user-friendly, and aesthetically pleasing websites and applications.
+       With my knowledge of HTML, CSS, JavaScript, Node.js, Express.js, Bootstrap, Wordpress, PHP, React, and REST API's, I'm capable of developing websites
+       and applications that are not only functional for businesses but enjoyable for users. </p> 
+       <p className='codingBrackets-Content'>With my creativity and hunger for learning the
+       latest technologies, I'm always on the hunt for new ways to innovate, and provide the best user experience for clients possible.</p>
+       </div>
+      <div className='introduction-Section'>
+       <h1 className='introduction-Header'>Hi, I'm Blake</h1>
+        <div className='header-Animation'>
             <span>I'm a </span>
             <TypeAnimation 
             sequence={[
@@ -24,14 +32,13 @@ function Header() {
             wrapper='span'
             repeat={Infinity}
             />
-      </div>
-      <p className='introduction-Content'>I'm an entry-level front-end developer with a passion for creating user-friendly, and aesthetically pleasing websites and applications.
-     With my knowledge of HTML, CSS, JavaScript, Node.js, Express.js, Bootstrap, Wordpress, PHP, React, and REST API's, I'm capable of developing websites
-      and applications that are not only functional for businesses but enjoyable for users. </p> 
-      <p className='introduction-Content'>With my creativity and hunger for learning the
+       </div>
+       <p className='introduction-Content'>I'm an entry-level front-end developer with a passion for creating user-friendly, and aesthetically pleasing websites and applications.
+       With my knowledge of HTML, CSS, JavaScript, Node.js, Express.js, Bootstrap, Wordpress, PHP, React, and REST API's, I'm capable of developing websites
+       and applications that are not only functional for businesses but enjoyable for users. </p> 
+       <p className='introduction-Content'>With my creativity and hunger for learning the
        latest technologies, I'm always on the hunt for new ways to innovate, and provide the best user experience for clients possible.</p>
      </div>
-     
     </div>
   )
 }
